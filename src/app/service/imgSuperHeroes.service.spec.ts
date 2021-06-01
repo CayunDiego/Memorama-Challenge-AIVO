8ba83780357a35d2fc37a9c3b3b1e4f08cc397a1
CayunDiego <cayun.diego.09@gmail.com>
2021-06-01 08:39:12 -0300

    [ADDED] - new image back card, styles cards

--- src/app/service/imgSuperHeroes.service.spec.ts
+++ src/app/service/imgSuperHeroes.service.spec.ts
@@ -3,7 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { ImgSuperHeroes } from './imgSuperHeroes.service';
 
 describe('ImgThesimpsonsService', () => {
-  let service: ImgThesimpsonsService;
+  let service: ImgSuperHeroes;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
