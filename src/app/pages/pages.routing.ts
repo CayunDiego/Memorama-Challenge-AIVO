554f0ad1f114138c73e3f203021e3bd7906e495e
CayunDiego <cayun.diego.09@gmail.com>
2021-06-01 01:27:57 -0300

    [SETUP] - initial configuration

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      {
+        path: 'game',
+        loadChildren: () =>
+          import('./game/game.component').then((m) => m.GameComponent)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
